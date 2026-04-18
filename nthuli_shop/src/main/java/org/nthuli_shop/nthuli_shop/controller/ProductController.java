5add3dbf838d88ff38e16c64077446282efcf9de
Brianoyaro <brianoyaro2000@yahoo.com>
2026-04-18 08:44:38 +0300

    added createProduct for the product controller file

--- nthuli_shop/src/main/java/org/nthuli_shop/nthuli_shop/controller/ProductController.java
+++ nthuli_shop/src/main/java/org/nthuli_shop/nthuli_shop/controller/ProductController.java
@@ -1,10 +1,14 @@
 package org.nthuli_shop.nthuli_shop.controller;
 
+import org.nthuli_shop.nthuli_shop.dto.ProductRequestDto;
 import org.nthuli_shop.nthuli_shop.dto.ProductResponseDto;
 import org.nthuli_shop.nthuli_shop.service.ProductService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import tools.jackson.databind.ObjectMapper;
 
 import java.util.HashMap;
 import java.util.List;
@@ -14,9 +18,11 @@ import java.util.Map;
 @RequestMapping("/api/products")
 public class ProductController {
 
+    private final ObjectMapper objectMapper;
     private final ProductService productService;
 
-    public ProductController(ProductService productService) {
+    public ProductController(ObjectMapper objectMapper, ProductService productService) {
+        this.objectMapper = objectMapper;
         this.productService = productService;
     }
 
@@ -50,9 +56,26 @@ public class ProductController {
         }
     }
 
-    @PostMapping("/create")
-    public void createProduct() {
+    @PostMapping(value = "/create", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> createProduct(
+            @RequestPart("product") String productJson,
+            @RequestPart("images") List<MultipartFile > images,
+            @RequestParam(defaultValue = "0") Integer primaryIndex
+    ) {
         // create with images
+        try {
+            ProductRequestDto request = objectMapper.readValue(productJson, ProductRequestDto.class);
+            ProductResponseDto repsonse = productService.createProduct(request, images, primaryIndex);
+            return ResponseEntity.ok(repsonse);
+        } catch (RuntimeException e) {
+            Map<String, String> error = new HashMap<>();
+            error.put("Message", e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+        } catch (Exception e) {
+            Map<String, String> error = new HashMap<>();
+            error.put("Message", "Internal server error" + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+        }
     }
 
     @PutMapping("/{id}")
